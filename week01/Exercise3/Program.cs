fb65e973abd8be6a6cb20bb992fd7945978a6dc5
AverageLehi <gom23011@byui.edu>
2025-09-05 11:10:56 +0800

    Exceeded requirement for Exercise 3 Program.cs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Hi! Welcome to Guess the Number game.");
+        bool continueGame = true;
+        string continueGameQ;
+        while(continueGame)
+        {
+            Console.WriteLine("I'm thinking of a magic number between 1-100.");
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1,100);
+            bool continuePlay = true;
+            int guessCount = 0;
+            while (continuePlay)
+            {
+                Console.WriteLine("What is the your guess?");
+                int guessNumber = int.Parse(Console.ReadLine());   
+
+                if (guessNumber == magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    guessCount += 1;
+                    Console.WriteLine($"You have tried {guessCount} time(s)!");
+                    continuePlay = false;
+                } 
+                else if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                    guessCount += 1;
+                }
+                else
+                {
+                    Console.WriteLine("Higher");
+                    guessCount += 1;
+                }
+
+            }
+            Console.WriteLine("Would you like to play again (yes/no)?");
+            continueGameQ = Console.ReadLine();
+            if (continueGameQ.ToLower() == "yes")
+            {
+                continuePlay = true;
+            }
+            else
+            {
+                continuePlay = false;
+            }
+        }
+
     }
 }
