ce9a3488362bc9278c21651c30a7f5a1e6e0e375
AverageLehi <gom23011@byui.edu>
2025-09-08 20:25:23 +0800

    Updated Week2 Program.cs, Job.cs, and Resume.cs

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Mircrosoft";
+        job1._startYear = 2022;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Google";
+        job2._startYear = 2025;
+        job2._endYear = 2029;
+
+        Resume myResume = new Resume();
+        myResume._name = "Lehi Gomez";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
+
     }
 }
