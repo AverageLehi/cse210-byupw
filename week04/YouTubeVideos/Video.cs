c0346cfa582025fa106a5ba7408d64cea4988a04
AverageLehi <gom23011@byui.edu>
2025-10-01 22:47:20 +0800

    Completed the Youtube video program.

---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,66 @@
+class Video
+{
+    private string _title;
+    private string _author;
+    private int _length;
+    private List<Comment> _comments;
+
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+        _comments = new List<Comment>();
+    }
+    public string GetTitle()
+    {
+        return _title;
+    }
+    public void SetTitle(string title)
+    {
+        _title = title;
+    }
+    public string GetAuthor()
+    {
+        return _author;
+    }
+    public void SetAuthor(string author)
+    {
+        _author = author;
+    }
+    public int GetLength()
+    {
+        return _length;
+    }
+    public void SetLength(int length)
+    {
+        _length = length;
+    }
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+    public int GetNumberOfComments()
+    {
+        return _comments.Count;
+    }
+    public List<Comment> GetComments()
+    {
+        return _comments;
+    }
+    public void DisplayVideoInfo()
+    {
+        Console.WriteLine($"Title: {GetTitle()}");
+        Console.WriteLine($"Author: {GetAuthor()}");
+        Console.WriteLine($"Length: {GetLength()} seconds");
+        Console.WriteLine($"Number of Comments: {GetNumberOfComments()}");
+
+        foreach (var comment in _comments)
+        {
+            Console.WriteLine("  - " + comment.Display());
+        }
+        Console.WriteLine();
+    }
+
+
+ }
